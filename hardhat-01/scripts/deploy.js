8acab2565c84db79ca30a4098a007730a4f877ac
sysOotsutsuki <mac@Macs-MacBook-Pro.local>
2023-03-02 23:34:27 +0700

    deploy and verified

--- hardhat-01/scripts/deploy.js
+++ hardhat-01/scripts/deploy.js
@@ -7,20 +7,17 @@
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Polkadex");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const [deployed] = await hre.ethers.getSigners();
+  const accountBalance = (await deployed.getBalance()).toString();
+  console.log('Account address deployed ', deployed.address);
+  console.log('Account balance: ', accountBalance);
+  const Polkadex = await hre.ethers.getContractFactory("Polkadex");
+  const lock = await Polkadex.deploy();
 
   await lock.deployed();
+  console.log('Address of toke Polkadex: ', lock.address);
 
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
